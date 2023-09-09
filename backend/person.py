bae3b84de526cfba8879a6b7af4832497d71b08c
kimiasami1378 <64108043+kimiasami1378@users.noreply.github.com>
2023-09-09 11:31:42 -0400

    #addi

--- backend/person.py
+++ backend/person.py
@@ -1,5 +1,13 @@
+import os
 import mysql.connector 
 import cgi 
+
+
+
+DB_HOST = os.environ.get('DB_HOST', 'localhost')  # Defaults to 'localhost' if not found
+DB_USER = os.environ.get('DB_USER', 'root')  # Defaults to 'root' if not found
+DB_PASSWORD = os.environ.get('DB_PASSWORD', '')  # Defaults to an empty string if not found
+DB_NAME = os.environ.get('DB_NAME', 'medihacks23')  # Defaults to 'medihacks23' if not found
 
 #make in individual methods
 form = cgi.FieldStorage() 
@@ -10,25 +18,33 @@ class Person:
     
     def __init__(self, username, name):
         self.WEB_ID = Person.tot_user 
-        Person.tot_user += Person.tot_user + 1
+        Person.tot_user += 1
         
         self.username = username 
         self.name = name
         self.institution = None 
         self.post_dict = {}
         
-        db = mysql.connector.connect(
-            host="localhost",
-            user="root", 
-            passwd="Tomh1015!",
-            database="medihacks23"
-            )
+        try:
+            db = mysql.connector.connect(
+                host=DB_HOST,
+                user=DB_USER,
+                passwd=DB_PASSWORD,
+                database=DB_NAME
+                )
+            
+            cursor = db.cursor()
 
-        cursor = db.cursor()
+            cursor.execute("INSERT INTO students (WEB_ID, username, name, institution) VALUES (%s, %s, %s, %s);", 
+                    (self.WEB_ID, self.username, self.name, self.institution))
+            db.commit()
 
-        cursor.execute("INSERT INTO students VALUES (%i, %s, %s, %s); ", 
-                (self.WEB_ID, self.username, self.name, self.institution))
-        
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+        finally:
+            cursor.close()
+            db.close()
+            
     
     def change_name(self, new_name):
         self.name = new_name
