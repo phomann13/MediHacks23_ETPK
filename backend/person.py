7d87859403a9a31d3323bf6721274a5055c764ce
Ely Cohen <elyanu@live.com>
2023-09-08 22:20:35 -0400

    worked on post file)

--- backend/person.py
+++ backend/person.py
@@ -27,3 +27,5 @@ class Person:
     
     def post(): #tbd 
     
+    
+    
---
+++ backend/post.py
@@ -0,0 +1,23 @@
+class Post:
+    
+    def __init__(self, ___):
+        
+        
+    def get_imptext(self):
+        return #get improvement text from post
+    
+    def get_comments(self):
+        return #get comments text from post
+    
+    def get_institution(self):
+        #capitalize text so it is standardized for all companies 
+        return #get institution from post
+    
+    def date_of_post(self) #necessary? 
+        return #date of post
+    
+    def ment_rating(self):
+        return #get mental health rating 
+    
+    def resource_rating(self):
+        return #get resource rating 
