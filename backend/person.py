c49bf2961aad63aca7d6cc4c41c3f9927a15b0a7
Ely Cohen <elyanu@live.com>
2023-09-08 21:55:55 -0400

    worked on person fie

--- backend/person.py
+++ backend/person.py
@@ -1,7 +1,29 @@
 class Person:
+    import #the file with the form info
+    form = #the file with the form info.FieldStorage()
+    #data1 = form.getvalue('__')
+    #data2 = form.getvalue('__')
+    #....
     
-    def __init__(self, name, id, age):
-        self.id = id 
+    def __init__(self, name, uid, day, month, year):
+        self.uid = uid 
         self.name = name
-        self.age = age
+        self.age_array = []
         
+        self.age_array.append(day)
+        self.age_array.append(month)
+        self.age_array.append(year)
+
+    def change_uid(self, new_uid):
+        self.uid = new_uid
+    
+    def change_name(self, new_name):
+        self.name = new_name
+    
+    def change_age(self, day, month, year):
+        self.age_array[0] = day
+        self.age_array[1] = month
+        self.age_array[2] = year
+    
+    def post(): #tbd 
+    
