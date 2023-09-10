bff3a9a5deb20a7e7b4832792056bcfb33efcc0d
Tom Hyland <tomhyland@wireless-10-105-37-81.umd.edu>
2023-09-10 15:18:02 -0400

    average functions for school ratings

--- analysis.js
+++ analysis.js
@@ -1,5 +1,4 @@
-/**
- * Post: 
+/** Post: 
  * {
  *  title: string,
  *  inst_name: string,
@@ -13,13 +12,52 @@
 
 //sample contains all from the JSON as an array of post objects
 const sample = require('./storage.json');
-array = [];
-posts = [];
+let array = [];
+let posts = [];
+let res_ratings = []; 
+let cult_ratings = []; 
 
 //stores all objects from JSON file into posts
 for (i = 0; i < sample.length; i ++) {
     array[i] = JSON.stringify(sample[i]);
     posts[i] = JSON.parse(array[i]);
+    if (res_ratings[posts[i].inst_name] == undefined){
+        res_ratings[posts[i].inst_name] = [parseInt(posts[i].res_rating)];
+    } else {
+        res_ratings[posts[i].inst_name][res_ratings[posts[i].inst_name].length] = parseInt(posts[i].res_rating);
+    }
+
+    if (cult_ratings[posts[i].inst_name] == undefined){
+        console.log("1");
+        cult_ratings[posts[i].inst_name] = [parseInt(posts[i].cult_rating)];
+    } else {
+        console.log("2");
+        cult_ratings[posts[i].inst_name][cult_ratings[posts[i].inst_name].length] = parseInt(posts[i].cult_rating);
+    }
 }
 
-console.log(posts[1].title);
+function get_school_cult_avg(school){
+    let sum = 0, count = cult_ratings[school].length;
+    if (count == 0) {
+        return 0;
+    }
+
+    for (i = 0; i < count; i++){
+        sum = sum + cult_ratings[school][i];
+    }
+    return sum/count; 
+}
+
+function get_school_res_avg(school){
+    let sum = 0, count = res_ratings[school].length;
+    if (count == 0) {
+        return 0;
+    }
+
+    for (i = 0; i < count; i++){
+        sum = sum + res_ratings[school][i];
+    }
+    return sum/count; 
+}
+
+console.log(get_school_cult_avg("UVA"));
