41d41f9a00680a7c5615582638750f0defa8f2b8
Tom Hyland <tomhyland@wireless-10-105-37-81.umd.edu>
2023-09-10 14:39:55 -0400

    got from storage to array

--- analysis.js
+++ analysis.js
@@ -1,11 +1,25 @@
 /**
- * 
+ * Post: 
+ * {
+ *  title: string,
+ *  inst_name: string,
+ *  cult_rating: int,
+ *  res_rating: int,
+ *  cult_comment: string,
+ *  res_comment: string
+ * }
  */
 
 
 //sample contains all from the JSON as an array of post objects
 const sample = require('./storage.json');
+array = [];
+posts = [];
 
+//stores all objects from JSON file into posts
+for (i = 0; i < sample.length; i ++) {
+    array[i] = JSON.stringify(sample[i]);
+    posts[i] = JSON.parse(array[i]);
+}
 
-
-console.log(sample[1].title);
+console.log(posts[1].title);
