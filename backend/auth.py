f5f072c0ebc9747b27a1f4b9d2431e0e13b8b5f6
Ely Cohen <elyanu@live.com>
2023-09-09 22:42:35 -0400

    having errors running the app at this point in time

--- backend/auth.py
+++ backend/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for 
+from .models import User 
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -28,7 +30,10 @@ def sign_up():
         elif len(password1) < 7:
             flash('Email must be greater than 6 characters.', cateogry='error')
         else:
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256')) 
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', cateogry='success')
-            # add user to database 
-            
+            return redirect(url_for('views.home'))
+        
     return render_template("signup.html")
