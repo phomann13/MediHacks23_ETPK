2a53b7659f435d1078bf9be9490339ce88e58de2
Tom Hyland <tomhyland@wireless-10-105-37-81.umd.edu>
2023-09-10 14:22:40 -0400

    more anaylsis updates

--- analysis.js
+++ analysis.js
@@ -1,2 +1,11 @@
+/**
+ * 
+ */
+
+
+//sample contains all from the JSON as an array of post objects
 const sample = require('./storage.json');
-console.log(sample.title);
+
+
+
+console.log(sample[1].title);
